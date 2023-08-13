64a3d4bd5b7213fca68468692eb7596952bd6a6e
Oleh Tytarenko <oleh.tytarenko@testpro.io>
2023-08-13 13:48:50 -0700

    Update LoginTests.java

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -17,8 +19,22 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://app.testpro.io/";
+        String url = "https://qa.koel.app/";
         driver.get(url);
+
+        WebElement emailInput = driver.findElement(By.cssSelector("[type='email']"));
+        emailInput.click();
+        emailInput.clear();
+        emailInput.sendKeys("demo@class.com");
+
+        WebElement passwordInput = driver.findElement(By.cssSelector("[type='password']"));
+        passwordInput.click();
+        passwordInput.clear();
+        passwordInput.sendKeys("te$t$tudent");
+
+        WebElement submitButton = driver.findElement(By.cssSelector("button[type='submit']"));
+submitButton.click();
+
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
     }
