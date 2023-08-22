e023ef2f3a95f0ec12067cf96506b65e73c52e1c
Oleh Tytarenko <oleh.tytarenko@testpro.io>
2023-08-21 22:22:20 -0700

    Update LogIn
    Removed quit method

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -20,6 +20,6 @@ public class LoginTests extends BaseTest {
         String url = "https://bbb.testpro.io/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+        
     }
 }
