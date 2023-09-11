9c1a271b29e1778e81408c76211a1ad15ac56697
Oleh Tytarenko <131427427+olehtytarenko@users.noreply.github.com>
2023-09-10 19:06:05 -0700

    Log in
    How to log in this my credintials

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,6 +1,9 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -17,9 +20,31 @@ public class LoginTests extends BaseTest {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://testpro.io/";
+        String url = "https://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        WebElement emailField = driver.findElement(By.cssSelector("[type='email']"));
+
+        emailField.click();
+        emailField.sendKeys("privet2@gmail.com");
+
+        WebElement passwordField = driver.findElement(By.cssSelector("[type='password']"));
+
+        passwordField.click();
+        passwordField.sendKeys("Privet1rive$");
+
+
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+
+        WebElement submitField = driver.findElement(By.cssSelector("[type='submit']"));
+        submitField.click();
+
+
+driver.quit();
     }
 }
