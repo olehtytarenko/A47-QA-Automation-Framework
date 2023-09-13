c0806777fd90813c10639bf20ae3654876281d9d
Oleh Tytarenko <131427427+olehtytarenko@users.noreply.github.com>
2023-09-12 22:59:56 -0700

    Homework 17 My own script

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,6 +1,8 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.interactions.Actions;
+
 
 public class BaseTest {
 
--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -3,13 +3,17 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class LoginTests extends BaseTest {
+public class LoginTests extends BaseTest
+{
     @Test
+
+
     public void LoginEmptyEmailPasswordTest() {
 
 //      Added ChromeOptions argument below to fix websocket error
@@ -22,20 +26,39 @@ public class LoginTests extends BaseTest {
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
 
-        WebElement registration = driver.findElement(By.xpath("//div/a"));
-        registration.click();
+        //WebElement registration = driver.findElement(By.xpath("//div/a"));
+        //registration.click();
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        WebElement email = driver.findElement(By.cssSelector("[name='email']"));
-        email.sendKeys("fgfgf@gmail.com");
+        WebElement email = driver.findElement(By.cssSelector("[type='email']"));
+        email.sendKeys("demo@gmail.com");
+
+        WebElement password = driver.findElement(By.cssSelector("[type='password']"));
+        password.sendKeys("te$t$tudent");
 
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        WebElement submit = driver.findElement(By.cssSelector("[type='submit']"));
+        WebElement submit = driver.findElement(By.xpath("//button"));
         submit.click();
 
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        driver.quit();
+        WebElement songs = driver.findElement(By.cssSelector("[href='#!/songs']"));
+        songs.click();
+
+        WebElement particularSong = driver.findElement(By.xpath("//*[@id=/'songsWrapper/']/div/div/div[1]/table/tr[3]/td[2]"));
+
+
+        // I Tried to make a script on myself. I stuck in the spot where I have to make an option click to the song.
+        // I found out to enable it I have to use Action Class to make double, drag and option clicks. Pls give advice how to implement Action class here
+WebElement addTo = driver.findElement(By.xpath("//li[@class='has-sub']"));
+addTo.click();
+WebElement favorites = driver.findElement(By.xpath("//li/ul/li[@class='favorite']"));
+favorites.click();
+
+//I dont know how to 'catch' pop-up window to get appropriate selector and then check it via assert method
+
+        }
     }
-}
+
